aef361723e017aff6a3a48be2275857dd46a8e6e
Wenke_Huang <591852399@qq.com>
2020-04-07 09:17:21 +0800

    4.7

--- bookstore-vue-admin-master/src/main.js
+++ bookstore-vue-admin-master/src/main.js
@@ -3,12 +3,9 @@ import Vue from 'vue'
 import App from './App'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
-//import './assets/theme/theme-green/index.css'
 import VueRouter from 'vue-router'
 import store from './vuex/store'
 import Vuex from 'vuex'
-//import NProgress from 'nprogress'
-//import 'nprogress/nprogress.css'
 import routes from './routes'
 import Mock from './mock'
 Mock.bootstrap();
@@ -18,7 +15,6 @@ Vue.use(ElementUI)
 Vue.use(VueRouter)
 Vue.use(Vuex)
 
-//NProgress.configure({ showSpinner: false });
 
 const router = new VueRouter({
   routes
